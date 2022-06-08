26da16fdb9e8c671a42205d4f6531aad9f4be7ec
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2022-06-08 15:00:39 -0300

    Inserindo o CRUD

--- Program.cs
+++ Program.cs
@@ -1,24 +1,11 @@
+using TodoList.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
